ca87351140a41c28064aca74ba4a2d25a0fb382f
EpicAdventuresRepos <hola.epicadventures@gmail.com>
2025-11-15 12:20:26 +0100

    Corregido error al cargar fichero de datos.

--- pilas_observer.py
+++ pilas_observer.py
@@ -5,23 +5,24 @@ import analysis as a3
 
 @st.cache_data
 def data():
-    df_master = pd.read_csv("../idealista_cvs/time_serie.csv")
+    df_master = pd.read_csv("time_serie_pilas.csv")
     df_pilas = df_master[df_master["Localidad"] == "Pilas"]
     return a3.dataframe_precios_medios_altas_bajas_mes(df_pilas)
 
 
 df = data()
 
+st.set_page_config(layout="wide")
 st.title("Observatorio de Pilas (Sevilla)\n")
-#st.subheader(f"Datos desde {df["Index"].min()} hasta {df["Index"].max()}.\n", divider="gray")
-st.subheader(f"Datos desde {df["Mes"].iloc[0]} hasta {df["Mes"].iloc[-1]}.", divider="gray")
-#st.space(size="small")
+st.space(size="small")
+st.subheader(f"Datos desde {df["Mes"].iloc[0].lower()} hasta {df["Mes"].iloc[-1].lower()} de 2025.", divider="gray")
+st.space(size="small")
 
 st.write("Media de anuncios por mes.")
 st.line_chart(df, x="Mes", y=["Media anuncios"], color=["#FF9896"])
+st.space(size="small")
 
 left_column, center_column, right_column = st.columns(3)
-
 with left_column:
     st.write("Nuevos anuncios y anuncios borrados por mes.")
     st.line_chart(df, x="Mes", y=["Nuevos", "Borrados"], color=["#D62728", "#FF9896"])
@@ -39,5 +40,6 @@ st.write("Resumen de datos.")
 st.table(data=df)
 
 st.subheader(f"", divider="gray")
-st.write("Última actualización 15/11/2025.")
+st.write("Última actualización: 15/11/2025.")
+st.write("X / Twitter: @InversorPython")
 
